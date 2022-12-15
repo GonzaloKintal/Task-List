6527c437f433ccafc0c5a85e9b06e679a89635ec
Gonzalo Kintal <kintalgonzalo40@gmail.com>
2022-12-15 14:27:47 -0300

    second commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../service/task.service';
+import {Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+    // Like promise
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks
+   });
+  }
+
+
+  deleteTask(task:Task) {
+    this.taskService.deleteTask(task)
+    .subscribe(
+      () => {
+        this.tasks = this.tasks.filter( (t) =>{
+          return t.id !== task.id
+        } )
+      })
+  }
+
+
+  toggleReminder(task:Task) {
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task) {
+    this.taskService.addTask(task).subscribe((task) => (
+      this.tasks.push(task)
+    ))
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Angular",
+        day: 'Agosto 5 a las 12:00hs',
+        reminder: true
+    },
+
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Agosto 6 a las 17:00hs',
+        reminder: true
+    },
+
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: 'Agosto 7 a las 19:00hs',
+        reminder: false
+    },
+
+    {
+        id: 4,
+        text: "Leer 50 páginas del libro actual",
+        day: 'Agosto 8 a las 10:00hs',
+        reminder: false
+    }
+]
